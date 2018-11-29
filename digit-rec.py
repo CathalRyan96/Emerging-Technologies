9d26f17316b65578e9273d239e9af205488fde61
CathalRyan96 <cathalryan48@gmail.com>
2018-11-29 10:44:21 +0000

    testing reading in files in digitrec script

--- digit-rec.py
+++ digit-rec.py
@@ -1,4 +1,27 @@
 # Import Libraries Needed
 import gzip
-import numpy as np 
-import keras as kr 
+import numpy as np
+import keras as kr
+
+
+def read_data():
+    # Start a neural network
+    model = kr.models.Sequential()
+
+    with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+        train_img = f.read()
+
+    with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+    with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+    with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+    print("File Type: ", type(train_img))
+    print(train_img[0:4])
+
+
+read_data()
