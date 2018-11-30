cf90872e809d7b81200371bd74f87992e66b1a7f
CathalRyan96 <cathalryan48@gmail.com>
2018-11-30 19:58:41 +0000

    working on tests

--- digit-rec.py
+++ digit-rec.py
@@ -47,12 +47,78 @@ def startNeuralNetwork():
     model = kr.models.Sequential()
 
     # Add a hidden layer with 1000 neurons and an input layer with 784.
-    model.add(kr.layers.Dense(units=500, activation='linear', input_dim=784))
+    model.add(kr.layers.Dense(units=500, activation='relu', input_dim=784))
     model.add(kr.layers.Dense(units=400, activation='relu'))
     model.add(kr.layers.Dense(units=200, activation='relu'))
 
     # Add a three neuron output layer.
     model.add(kr.layers.Dense(units=10, activation='softmax'))
 
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+        train_img = f.read()
+
+    with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+    # Read all the imported folders
+    train_img = ~np.array(list(train_img[16:])).reshape(
+        60000, 28, 28).astype(np.uint8) / 255.0
+    train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
+
+    # Reshape train_img/ changes it to an array
+    inputs = train_img.reshape(60000, 784)
+
+    encoder = pre.LabelBinarizer()
+    encoder.fit(train_lbl)
+    outputs = encoder.transform(train_lbl)
+
+    print(train_lbl[0], outputs[0])
+
+    model.fit(inputs, outputs, epochs=10, batch_size=100)
+    model.save("data/saveModel.h5")
+
+
+def Tests():
+    with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+        train_img = f.read()
+
+    with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+     # Read all the imported folders
+    train_img = ~np.array(list(train_img[16:])).reshape(
+        60000, 28, 28).astype(np.uint8) / 255.0
+    train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
+
+    # Start a neural network, building it by layers.
+    model = kr.models.Sequential()
+
+    # Add a hidden layer with 1000 neurons and an input layer with 784.
+    model.add(kr.layers.Dense(units=500, activation='relu', input_dim=784))
+    model.add(kr.layers.Dense(units=400, activation='relu'))
+    model.add(kr.layers.Dense(units=200, activation='relu'))
+
+    # Add a three neuron output layer.
+    model.add(kr.layers.Dense(units=10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    encoder = pre.LabelBinarizer()
+    encoder.fit(train_lbl)
+    outputs = encoder.transform(train_lbl)
+
+    for i in range(10):
+        print(i, encoder.transform([i]))
+
+    print(encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum()
+
 
 read_data()
+
+startNeuralNetwork()
+
+Tests()
