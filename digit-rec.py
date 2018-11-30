4a3bed6cff191b301215061f7db762afac01ad24
CathalRyan96 <cathalryan48@gmail.com>
2018-11-30 17:36:10 +0000

    reading in imported folders and reshaping

--- digit-rec.py
+++ digit-rec.py
@@ -20,8 +20,20 @@ def read_data():
     with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
         t10k_img = f.read()
 
-    print("File Type: ", type(train_img))
-    print(train_img[0:4])
+    print("Testing to see File type: ", type(train_img))
+    print("Testing to see if first 4 bytes are printed out: ", train_img[0:4])
+
+    # Read all the imported folders
+    train_img = ~np.array(list(train_img[16:])).reshape(
+        60000, 28, 28).astype(np.uint8) / 255.0
+    train_lbl = np.array(list(train_lbl[8:])).astype(np.uint8)
+
+    t10k_img = ~np.array(list(t10k_img[16:])).reshape(
+        60000, 28, 28).astype(np.uint8) / 255.0
+    t10k_lbl = np.array(list(t10k_lbl[8:])).astype(np.uint8)
+
+    # Reshape train_img
+    inputs = train_img.reshape(60000, 784)
 
 
 read_data()
